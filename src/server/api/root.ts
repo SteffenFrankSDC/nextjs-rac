510411850df83daf88cf38aae56d87dd2877e520
Steffen Frank <sfrank@weevolve.dev>
2023-04-10 15:40:03 +0200

    add UI for topics, fix db schema

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,15 +1,15 @@
-import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { createTRPCRouter } from "./trpc";
+import { exampleRouter } from "./routers/example";
 import { topicRouter } from "./routers/topic";
 
 /**
  * This is the primary router for your server.
  *
- * All routers added in /api/routers should be manually added here.
+ * All routers added in /api/routers should be manually added here
  */
 export const appRouter = createTRPCRouter({
   example: exampleRouter,
-  topic: topicRouter
+  topic: topicRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/topic.ts
+++ src/server/api/routers/topic.ts
@@ -1,19 +1,15 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const topicRouter = createTRPCRouter({
-  getAll: protectedProcedure
-    .query(({ ctx }) => {
-      return ctx.prisma.topic.findMany({
-        where: {
-          userId: ctx.session.user.id
-        }
-      })
-    }),
+  getAll: protectedProcedure.query(({ ctx }) => {
+    return ctx.prisma.topic.findMany({
+      where: {
+        userId: ctx.session.user.id,
+      },
+    });
+  }),
 
   create: protectedProcedure
     .input(z.object({ title: z.string() }))
@@ -21,8 +17,8 @@ export const topicRouter = createTRPCRouter({
       return ctx.prisma.topic.create({
         data: {
           title: input.title,
-          userId: ctx.session.user.id
-        }
-      })
-    })
+          userId: ctx.session.user.id,
+        },
+      });
+    }),
 });
